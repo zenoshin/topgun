0fd79b71a8a1f2d5a71c53683b42ad3f7bc8e9b7
Zeno Shin <zenoshin15@gmail.com>
2015-12-14 18:32:43 -0500

    change text color by frame rate

--- frameRate.cpp
+++ frameRate.cpp
@@ -22,15 +22,18 @@ void FrameRate::draw(sf::RenderWindow& window)
 	if(clock.getElapsedTime().asSeconds() >= 1.f)
 	{
 		frameRateText.setString(std::to_string(frameCount) + "fps");
+		frameRateText.setColor(frameCount >= 30 ? sf::Color::Green :
+				frameCount >= 10 ? sf::Color::Yellow : sf::Color::Red);
 		clock.restart();
 		frameCount = 0;
 	}
 	frameRateText.setOrigin(frameRateText.getLocalBounds().width + 6, 0);
 	const unsigned int windowWidth = window.getSize().x;
 	frameRateText.setPosition(windowWidth + 1, 1);
+	const auto orgColor = frameRateText.getColor();
 	frameRateText.setColor(sf::Color(0, 0, 0, 128));
 	window.draw(frameRateText);
+	frameRateText.setColor(orgColor);
 	frameRateText.setPosition(windowWidth, 0);
-	frameRateText.setColor(sf::Color(224, 224, 224, 255));
 	window.draw(frameRateText);
 }
